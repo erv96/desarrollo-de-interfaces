af5b3c952d80be8c34c46663b9359e6bd79d7f98
David <toledo18696@gmail.com>
2022-11-14 14:23:11 +0100

    completando prSceneBuilder

--- prSceneBuilder/src/application/SampleController.java
+++ prSceneBuilder/src/application/SampleController.java
@@ -1,5 +1,29 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class SampleController {
+	
+	@FXML
+	private TextField txtMensaje;
+	
+	@FXML
+	private Button btnMensaje;
+	
+	@FXML
+	private Label lblMensaje;
+	
+	@FXML
+	public void mostrarMensaje(ActionEvent event) {
+		lblMensaje.setText(txtMensaje.getText());
+	}
+	
+	public void enviarMensaje(ActionEvent event) {
+		lblMensaje.setText(txtMensaje.getText());
+	}
 	
 }
