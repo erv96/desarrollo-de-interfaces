15ec0360aca2f4f5bde478e289c1062d527bb4d7
David <toledo18696@gmail.com>
2022-12-07 18:17:46 +0100

    haciendo el proyecto videojuegos

---
+++ FXML_ejercicio_2_videojuegos/src/application/Ejercicio2.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Ejercicio2 extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,600,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Videojuegos");
+			primaryStage.getIcons().add(new Image("Icon/game.png"));
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
